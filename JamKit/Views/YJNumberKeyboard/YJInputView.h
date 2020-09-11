0a306644709430923b0160936b2f72ed6b6bb1c2
张文洁 <50493215@qq.com>
2020-09-11 17:29:43 +0800

    (*)上传基础代码

--- JamKit/Views/YJNumberKeyboard/YJInputView.h
+++ JamKit/Views/YJNumberKeyboard/YJInputView.h
@@ -7,10 +7,46 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MMNumberKeyboard.h>
+
+@class YJInputView;
 
 NS_ASSUME_NONNULL_BEGIN
 
+// 点击的代理事件 传递到外层
+@protocol YJInputViewDelegate <NSObject>
+// 每次输入的代理事件
+-(void)inputViewDidChangeText:(YJInputView *)inputView;
+
+@end
+
 @interface YJInputView : UIView
+
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, copy) NSMutableString *placeholderText;
+@property (nonatomic, copy) NSString *trueText;
+@property (nonatomic, strong) NSString *textTag;
+@property (nonatomic, weak) id<YJInputViewDelegate> InputViewDelegate;
+
+/**
+ *  带字母 字符 数字切换工具条
+ */
++(YJInputView *)shareKBInputViewWithTypeAll;
+/**
+ *  数字
+ */
++(YJInputView *)shareKBInputViewWithTypeNum;
+/**
+ *  数字+小数点
+ */
++(YJInputView *)shareKBInputViewWithTypeNumDecimal;
+/**
+ *  字母+符号
+ */
++(YJInputView *)shareKBInputViewWithTypeABC;
+
+// 显示
+-(void)show;
 
 @end
 
